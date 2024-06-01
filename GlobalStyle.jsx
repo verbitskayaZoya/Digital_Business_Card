af3b42669f99cab24fe8fb181448f79ca63f64be
verbitskayaZoya <103507783+verbitskayaZoya@users.noreply.github.com>
2024-06-01 21:10:25 +0700

    fixup! fix img src

--- GlobalStyle.jsx
+++ GlobalStyle.jsx
@@ -1,14 +1,19 @@
 import { createGlobalStyle } from "styled-components"
 
 export const GlobalStyle = createGlobalStyle`
-  body {
+ * {
+    box-sizing: border-box;
+  }
+
+body {
     background-color: #23252C;
     color: whitesmoke;
-    height: 100vh;
+    min-height: 100vh;
     font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
     display: flex;
     justify-content: center;
     align-items: center;
+    margin: 0;
 }
 `
 
